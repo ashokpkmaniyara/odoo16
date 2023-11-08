153f3b0e992cca096f42cad7382833c733cc3af4
ashokpk <ashokpkmaniyara@gmail.com>
2023-11-08 12:57:47 +0530

    date in o2m field completed

--- my_addons/date_in_o2m_field/__manifest__.py
+++ my_addons/date_in_o2m_field/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 {
     'name':'Date in O2M field',
     'version':'16.0.1.0.0',
@@ -8,6 +9,8 @@
     Date in o2m field in project form
     """,
     'data':[
-        'views/o2m_field_view.xml'
+        'security/ir.model.access.csv',
+        'views/o2m_field_view.xml',
+        'views/date_fields_view.xml',
     ]
 }
--- my_addons/date_in_o2m_field/models/__init__.py
+++ my_addons/date_in_o2m_field/models/__init__.py
@@ -1,1 +1,2 @@
 from . import o2m_field
+from . import date_fields
